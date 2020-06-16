9def92ec3dda2d40ddd269e22787bd449484134b
oluSammy <olumorinsammy@gmail.com>
2020-06-15 18:24:05 -0700

    finished testimonial section

--- js/app.js
+++
@@ -1,7 +0,0 @@
-// const carouselSlide = document.querySelector('.stories__slide');
-// const carouselImages = document.querySelectorAll('.story');
-
-// let counter = 1;
-// const size = carouselImages[0].clientWidth;
-
-// carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
