d28d720bc1af8c0263a6a7b9f8cc30f0fa9c635e
oluSammy <olumorinsammy@gmail.com>
2020-06-15 18:20:38 -0700

    completed client-testimonial section

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// const carouselSlide = document.querySelector('.stories__slide');
+// const carouselImages = document.querySelectorAll('.story');
+
+// let counter = 1;
+// const size = carouselImages[0].clientWidth;
+
+// carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
